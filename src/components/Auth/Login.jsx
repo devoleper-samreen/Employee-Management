5be458d8d9f834b8347a86063205468a56315cdc
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-26 17:51:30 +0530

    handle login form

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log("submitted!");
+        console.log('email is : ', email);
+        console.log('password is :', password);
+
+        setEmail('');
+        setPassword("");
+
+
+
+    }
     return (
-        <div className="flex items-center justify-center h-screen w-full px-5 sm:px-0">
+        <form onSubmit={submitHandler} className="flex items-center justify-center h-screen w-full px-5 sm:px-0">
             <div className="flex bg-[#272727] rounded-lg shadow-lg overflow-hidden max-w-sm lg:max-w-4xl w-full">
                 <div
                     className="hidden md:block lg:w-1/2 bg-cover bg-blue-700"
                     style={{
                         backgroundImage: `url(https://www.tailwindtap.com//assets/components/form/userlogin/login_tailwindtap.jpg)`,
                     }}
-                ></div>
+                >
+
+                </div>
                 <div className="w-full p-8 lg:w-1/2">
                     <p className="text-xl text-gray-600 text-center">Welcome back!</p>
                     <div className="mt-4">
-
                         <div className="flex justify-between">
                             <label className="block text-gray-700 text-sm font-bold mb-2">
                                 Email Address
@@ -24,6 +41,8 @@ const Login = () => {
                             type="email"
                             placeholder="Enter Email here"
                             required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                     </div>
                     <div className="mt-4 flex flex-col justify-between">
@@ -36,21 +55,24 @@ const Login = () => {
                             className="text-gray-700 border border-gray-300 rounded py-2 px-4 block w-full focus:outline-2 focus:outline-blue-700"
                             type="password"
                             placeholder="Enter password here"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
                         />
-                        <a
-                            href="#"
+                        <Link
+
                             className="text-xs text-gray-500 hover:text-gray-900 text-end w-full mt-2"
                         >
                             Forget Password?
-                        </a>
+                        </Link>
                     </div>
                     <div className="mt-8">
-                        <button className="bg-blue-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-600">
+                        <button type='submit' className="bg-blue-700 text-white font-bold py-2 px-4 w-full rounded hover:bg-blue-600 cursor-pointer">
                             Login
                         </button>
                     </div>
-                    <a
-                        href="#"
+                    <Link
+
                         className=" flex items-center justify-center mt-4 text-white rounded-lg shadow-md hover:bg-gray-100"
                     >
                         <div className="flex px-5 justify-center w-full py-3">
@@ -80,19 +102,18 @@ const Login = () => {
                                 </h1>
                             </div>
                         </div>
-                    </a>
+                    </Link>
                     <div className="mt-4 flex items-center w-full text-center">
-                        <a
-                            href="#"
+                        <Link
                             className="text-xs text-gray-500 capitalize text-center w-full"
                         >
                             Don&apos;t have any account yet?
                             <span className="text-blue-700"> Sign Up</span>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     );
 };
 export default Login;
