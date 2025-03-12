5f071920201011431344d016d5f686adf6cc53c7
devoleper-samreen <samreenmalik52292@gmail.com>
2025-03-12 12:49:54 +0530

    feat: add logout functionality to header component with navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import useAuthStore from "../../store/authStore"
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
-    const { user } = useAuthStore()
-    console.log("login user : ", user);
+    const navigate = useNavigate()
+    const { user, logout } = useAuthStore()
 
+    const onLogout = () => {
+        logout()
+        localStorage.removeItem("Auth-storage")
+        navigate('/')
+    }
 
     return (
         <div className='flex items-center justify-between h-20 px-4'>
             <div className='text-xl'>Hello <br /><span className='text-amber-500 text-3xl font-bold'>{user?.name || "User"} 👋</span>
             </div>
             <div>
-                <button className='bg-blue-500 px-8 py-3 text-center rounded font-bold text-lg cursor-pointer'>logout</button>
+                <button onClick={onLogout} className='bg-blue-500 px-8 py-3 text-center rounded font-bold text-lg cursor-pointer'>logout</button>
             </div>
         </div>
     )
